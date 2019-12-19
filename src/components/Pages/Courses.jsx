c09d3ef13f258c85b1bcc593d2be9f4147735df3
Angel <angel.sosa@gdpteam.com>
2019-12-19 17:57:30 -0500

    se crea todos los componentes de las paginas privadas de la aplicacion

---
+++ src/components/Pages/Courses.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Courses = () => (
+  <div className="ed-grid">
+    <h1>Pagina de cada curso</h1>
+  </div>
+)
+
+export default Courses
---
+++ src/components/Pages/Teachers.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Teachers = () => {
+  return (
+    <div className="ed-grid">
+      <h1>Profesores</h1>
+    </div>
+  )
+}
+
+export default Teachers
+
